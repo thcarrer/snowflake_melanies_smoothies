cca972acd9a1cccdc3f7cae856d8365b7f730914
thcarrer <thomas.carrer@keyrus.com>
2026-03-31 10:56:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col
+from snowflake.snowpark import Session
 import requests
 
 # Write directly to the app.
@@ -17,7 +18,7 @@ st.write("The name on your Smoothie will be", name_on_order)
 
 
 #session = get_active_session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
