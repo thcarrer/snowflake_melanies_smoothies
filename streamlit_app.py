c09a2a213627e1703aa17124156172e47e3c5b9e
thcarrer <thomas.carrer@keyrus.com>
2026-03-31 11:00:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-from snowflake.snowpark import Session
+from snowflake.snowpark import session
 
 # Write directly to the app.
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
