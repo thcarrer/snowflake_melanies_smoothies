a2a190667b84f16bde982d23cfa0f21348b8ad49
thcarrer <thomas.carrer@keyrus.com>
2026-03-31 14:35:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ st.write("The name on your Smoothie will be", name_on_order)
 
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, width='stretch')
-st.stop()
+#st.dataframe(data=my_dataframe, width='stretch')
+#st.stop()
 
 
 ingredients_list = st.multiselect(
